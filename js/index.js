d25e4101094b986d719bdc08542599ef8c2340eb
Joshix-1 <dingden04@gmail.com>
2020-06-27 23:25:39 +0000

    , -> ;

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ function setzeAlleFelder (euroWert, ignoriert) {
         }
     }
 
-    output.value = (felder[0].value + " Euro, das sind ja " + felder[1].value + " Mark, " + felder[2].value + " Ostmark und " + felder[3].value + " Ostmark auf dem Schwarzmarkt!").replace(/\./g, ",").replace(/,00/g, "");
+    output.value = (felder[0].value + " Euro, das sind ja " + felder[1].value + " Mark; " + felder[2].value + " Ostmark und " + felder[3].value + " Ostmark auf dem Schwarzmarkt!").replace(/\./g, ",").replace(/,00/g, "");
 }
 
 for (let i = 0; i < 4; i++) {
